93c092f279ae0a2cac119c435fd2573a1ff5ef26
Jack Skrable <jskrable@bu.edu>
2019-01-31 09:14:03 -0500

    refactored into strategy and summarize scripts

---
+++ hw1/strategy.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+strategies.py
+01-31-2019
+jack skrable
+"""
+
+import os
+import summarize as s
+ 
+ticker = 'SYK'
+
+# Get path of dataset
+if os.name == 'posix':
+    input_dir = r'/home/jskrable/code/cs677/datasets'
+else:
+    input_dir = r'C:\Users\jskrable\code\cs677\datasets'
+output_file = os.path.join(input_dir, ticker + '.csv')
+
+# Read data file
+with open(output_file) as f:
+    lines = f.read().splitlines()
+
+    # Run summarize for day and month
+    s.summary(lines, 'day')
+    s.summary(lines, 'month')
---
+++ hw1/summarize.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+summarize.py
+01-31-2019
+jack skrable
+"""
+
+
+# Function to summarize % change is adj. close if held for a specific interval
+def summary(lines, interval):
+
+	# Omit header row
+	lines = lines[1:]
+	# Init dicts to store data
+	data = {}
+	summary = {}
+
+	# Loop through lines in dataset
+	for line in lines:
+		# Split csv string
+		row = line.split(',')
+		# Choose time span to hold stock for
+		hold = row[4] if interval == 'day' else row[2]
+		# Get adj. closing price
+		price = float(row[9])
+
+		# Populate prices array for each holding period
+		try:
+			data[hold]['prices'].append(price)
+			pass
+		except KeyError as e:
+			data.update({hold: {'prices': [price]}})
+
+
+	# Loop through holding periods
+	for key in data:
+
+		prices = data[key]['prices']
+		changes = []
+		# Loop through prices for period
+		for i, val in enumerate(prices):
+			# Ignore first price
+			change = 0 if i == 0 else ((val - prices[i-1])/prices[i-1])
+			changes.append(change)
+
+		# Add changes to holding period
+		data[key].update({'changes': changes})
+		
+		# print('DATA--------------')
+		# print(data)
+
+		# Get stats for changes list
+		minimum = min(data[key]['changes'])
+		maximum = max(data[key]['changes'])
+		avg = (sum(data[key]['changes'])/len(data[key]['changes']))
+		med = median(data[key]['changes'])
+
+		# Add to summary dict
+		summary.update({key: {'summary': {'min': minimum, 'max': maximum, 'average': avg, 'median': med}}})
+
+	print('Data -----------------------')
+	print(summary)
+		
+
+# Function to get median value of a list
+def median(data):
+
+	data.sort()
+	mid = int((len(data)-1)/2)
+
+	if len(data) % 2:
+		return data[mid]
+	else:
+		return (data[mid-1] + data[mid])/2
