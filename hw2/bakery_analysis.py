e3399e25c7ae93461a06a46689f7ad181b4a379a
Jack Skrable <jskrable@gmail.com>
2019-02-10 14:19:17 -0500

    busiest and most profitable intervals done

--- hw2/bakery_analysis.py
+++ hw2/bakery_analysis.py
@@ -19,10 +19,40 @@ output_file = os.path.join(input_dir, 'BreadBasket_DMS_output.csv')
 # Read file into pandas dataframe
 data = pd.read_csv(output_file)
 
-period = data.groupby('Period')
+
+def busiest(data, group):
+
+	interval = data.groupby(group).agg({'Transaction':'count'})
+	interval_maxkey = interval['Transaction'].idxmax()
+	interval_max = interval['Transaction'].max()
+
+	print('Busiest interval for',group,'was',interval_maxkey,'with',interval_max,'transactions.')
+
+def profitable(data, group):
+
+	interval = data.groupby(group).agg({'Item_Price':'sum'})
+	interval_maxkey = interval['Item_Price'].idxmax()
+	interval_max = interval['Item_Price'].max().round(2)
+	print('Most profitable interval for',group,'was',interval_maxkey,'with',interval_max,'in revenue.')
+
+print('Busiest-------------------------------------------------------------')
+busiest(data, 'Period')
+busiest(data, 'Hour')
+busiest(data, 'Weekday')
+print('Most Profitable-----------------------------------------------------')
+profitable(data, 'Period')
+profitable(data, 'Hour')
+profitable(data, 'Weekday')
+
 hour = data.groupby('Hour')
 day = data.groupby('Weekday')
 
-print(period['Item_Price'].sum())
-print(hour['Item_Price'].sum())
-print(day['Item_Price'].sum())
+# print('Busiest -----------------------')
+# print(max(hour['Transaction'].count()).key())
+# print(hour['Transaction'].count())
+# print(day['Transaction'].count())
+# print(period['Transaction'].count())
+# print('Most Profitable----------------')
+# print(hour['Item_Price'].count())
+# print(day['Item_Price'].count())
+# print(period['Item_Price'].count())
