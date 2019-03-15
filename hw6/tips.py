7f470c6a4b0758450fb43dc40c3325c30818c89b
Jack Skrable <jskrable@bu.edu>
2019-03-15 16:27:05 -0400

    need to finish 6 and 7

--- hw6/tips.py
+++ hw6/tips.py
@@ -42,9 +42,14 @@ print(day_meal_avg,'\n')
 
 #q3
 print('Q3 ---------------------------------------------------------')
-df_tt = df[['tip_percent','total_bill']]
-corr = df_tt.corr().total_bill.tip_percent
+corr = df[['tip_percent','total_bill']].corr().total_bill.tip_percent
 print('The correlation between total bill price and tip is',corr,'\n')
+
+#q4
+print('Q4 ---------------------------------------------------------')
+df_tip_size = df[['tip_percent','size']].rename(index=str, columns={'size': 'group_size'})
+corr = df_tip_size.corr().group_size.tip_percent
+print('The correlation between group size and tip is',corr,'\n')
 
 #q5
 print('Q5 ---------------------------------------------------------')
@@ -56,7 +61,22 @@ print(nonsmoker,'% of patrons are non-smokers\n')
 
 #q8
 print('Q8 ---------------------------------------------------------')
-df_ts = df[['tip_percent','smoker']]
-df_ts['smoker'] = df_ts['smoker'].apply(lambda x: 1 if x == 'Yes' else 0)
-corr = df_ts.corr().smoker.tip_percent
+df_tip_smoker = df[['tip_percent','smoker']]
+df_tip_smoker['smoker'] = df_tip_smoker['smoker'].apply(lambda x: 1 if x == 'Yes' else 0)
+corr = df_tip_smoker.corr().smoker.tip_percent
 print('The correlation between smokers and tipping is',corr,'\n')
+
+#q9
+print('Q9 ---------------------------------------------------------')
+day_avg = df.groupby('day')['tip_percent'].mean()
+print('The average tip is highest on',day_avg.idxmax(),'\n')
+print(day_avg)
+
+#q10
+print('Q10 --------------------------------------------------------')
+females = df[df.sex.str.contains('Female')]
+f_smokers = np.round((len(females[females.smoker.str.contains('Yes')]) / len(females)) * 100, 2)
+males =  df[df.sex.str.contains('Male')]
+m_smokers = np.round((len(males[males.smoker.str.contains('Yes')]) / len(males)) * 100, 2)
+print(f_smokers,'% of females are smokers')
+print(m_smokers,'% of males are smokers')
