8a74ccbcfe947de0f9f48eb05b33a011b6275b14
Jack Skrable <jskrable@gmail.com>
2019-03-16 09:22:03 -0400

    first implementation

--- hw6/overnight.py
+++ hw6/overnight.py
@@ -24,8 +24,50 @@ output_file = os.path.join(input_dir, ticker + '.csv')
 print('Reading data file...')
 df = pd.read_csv(output_file)
 
-subset = df[['Open','Close']]
 
-x = subset.iloc[10]
+# Helper function to calculate overnight return for a row
+def overnight(row):
+    prev_close = df.iloc[row.name-1].Close
+    return (row.Open - prev_close) / prev_close
 
-y = subset.iloc(x.name -1)
+# Add overnight return column
+df['Overnight'] = df.apply(overnight, axis=1)
+
+# Drop first row with bad value for overnight
+df = df[1:]
+
+shares = 0
+results = {}
+
+
+def on_inertia(row):
+    global shares
+    if shares == 0 and row.Overnight > 0:
+        shares = 100 / row.Open
+
+    elif shares > 0 and row.Overnight <= 0:
+        net = (row.Open -row.Close) / shares
+        shares = 0
+        results.update({row.name : net})
+
+df.apply(on_inertia)
+
+
+def plot_returns(df):
+
+    x = df.Overnight.values
+    y = df.Return.values
+    plt.scatter(x,y)
+    plt.show()
+
+
+def plot_results(results):
+
+    x = [x for x in results.keys()]
+    y = [y for y in results.values()]
+    plt.plot(x,y)
+    plt.show()
+
+
+
+
