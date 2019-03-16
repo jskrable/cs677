00acdce11ddbf1dbb6c3b018ac235b613ee2a948
Jack Skrable <jskrable@gmail.com>
2019-03-16 10:45:09 -0400

    Q3 done, need to refine output for Q1 and Q2

--- hw6/overnight.py
+++ hw6/overnight.py
@@ -11,6 +11,75 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+# Helper function to calculate overnight change for a row
+def overnight_change(row):
+    return row.Open - df.iloc[row.name-1].Close
+
+
+# Helper function to calc overnight return for a row
+def overnight_return(row):
+    prev_close = df.iloc[row.name-1].Close
+    return (row.Open - prev_close) / prev_close
+
+
+# Trading strategy based on overnight price change inertia
+def on_inertia(row, R):
+    global shares
+    if shares == 0 and row['Overnight_Change'] > R:
+        shares = 100 / row.Open
+
+    elif shares > 0 and row['Overnight_Change'] <= R:
+        net = (row.Open -row.Close) / shares
+        shares = 0
+        # return_df.append({'date': row.Date, 'return': net}, ignore_index=True)
+        return net
+
+
+# Runs overnight inertia strategy without any minimum change parameter
+def naive_on_inertia(df):
+    returns = df.apply(lambda x: on_inertia(x, 0), axis=1)
+    returns = returns[returns.isnull() == False]
+    return returns
+
+
+# Plots return columns from dataframe
+def plot_returns(df):
+    x = df['Overnight_Return'].values
+    y = df['Return'].values
+    plt.scatter(x,y)
+    plt.xlabel('Overnight Returns')
+    plt.ylabel('Daily Returns')
+    plt.title('Daily Returns vs. Overnight Returns')
+    plt.show()
+
+
+# Plots results of strategy from dict
+def plot_results(results):
+    x = [x for x in results.keys()]
+    y = [y for y in results.values()]
+    plt.plot(x, y, 'o:')
+    plt.ylabel('Mean Return')
+    plt.xlabel('R-value')
+    plt.title('Mean Return vs. R-value')
+    plt.show()
+
+
+# Runs overnight inertia strategy with a range of min change parameters
+def minimum_rs(df):
+    Rs = [r for r in range(-10,11)]
+    results = {}
+    for R in Rs:
+        print('Simulating with R:',R)
+        returns = df.apply(lambda x: on_inertia(x, R), axis=1)
+        returns = returns[returns.isnull() == False]
+        results.update({R: returns.mean()})
+
+    return results
+
+
+# MAIN
+################################################################
 ticker = 'SYK'
 
 # Get path of set
@@ -24,50 +93,30 @@ output_file = os.path.join(input_dir, ticker + '.csv')
 print('Reading data file...')
 df = pd.read_csv(output_file)
 
-
-# Helper function to calculate overnight return for a row
-def overnight(row):
-    prev_close = df.iloc[row.name-1].Close
-    return (row.Open - prev_close) / prev_close
-
 # Add overnight return column
-df['Overnight'] = df.apply(overnight, axis=1)
+print('Processing overnight columns...')
+df['Overnight_Return'] = df.apply(overnight_return, axis=1)
+df['Overnight_Change'] = df.apply(overnight_change, axis=1)
 
 # Drop first row with bad value for overnight
 df = df[1:]
 
+# Globals
 shares = 0
 results = {}
 
+# Q1
+print('Q1: Naive Overnight Inertia --------------------------------------------')
+naive = naive_on_inertia(df)
+# plot_returns(naive)
 
-def on_inertia(row):
-    global shares
-    if shares == 0 and row.Overnight > 0:
-        shares = 100 / row.Open
+#Q2
+print('Q2: Algorithm Analysis -------------------------------------------------')
+print('Displaying plot...')
+plot_returns(df)
 
-    elif shares > 0 and row.Overnight <= 0:
-        net = (row.Open -row.Close) / shares
-        shares = 0
-        results.update({row.name : net})
-
-df.apply(on_inertia)
-
-
-def plot_returns(df):
-
-    x = df.Overnight.values
-    y = df.Return.values
-    plt.scatter(x,y)
-    plt.show()
-
-
-def plot_results(results):
-
-    x = [x for x in results.keys()]
-    y = [y for y in results.values()]
-    plt.plot(x,y)
-    plt.show()
-
-
-
-
+#Q3
+print('Q3: Minimum Return Overnight Inertia -----------------------------------')
+R_results = minimum_rs(df)
+print('Displaying plot...')
+plot_results(R_results)
