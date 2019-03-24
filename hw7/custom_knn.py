fde9679c66fb6b2dc7c6951866ff733e48f264df
Jack Skrable <jskrable@gmail.com>
2019-03-24 18:21:32 -0400

    comments

--- hw7/custom_knn.py
+++ hw7/custom_knn.py
@@ -99,38 +99,56 @@ test = weekly[:int(len(weekly)/2)]
 # Custom knn, takes in X and returns predicted Y
 def custom_knn(train, to_predict, o, k):
 
+    # Init list for distances
     distances = []
+    # Loop thru all training values
     for row in train.values:
+        # Measure distance to prediction point
         dist = np.linalg.norm((row[:-1] - to_predict), ord=o)
+        # Add distance and label to list
         distances.append([dist, row[-1]])    
 
+    # Get label for k closest neighbors
     results = [dist[1] for dist in sorted(distances)[:k]]
+    # Return the highest prob. label
     return mode(results)
 
 
 # Try ks, get error rate
 error_rate = {}
 Ks = [x for x in range(1, 22, 2)]
+# List of ords to try
+# Manhattan, minikowski, and euclidean 
 Os = [1,1.5,2]
 size = len(test)
 
+# Try all k values
 for k in Ks:
+    # With each distance calculation
     for o in Os:
         key = '-'.join([str(k),str(o)])
         print('Classifying with k:', k, 'ord:', o)
+        # Init bad prediction counter
         error = 0
+        # Loop thru test rows
         for row in test.values:
+            # Make a prediction
             pred = custom_knn(train, row[:-1], o, k)
             if pred != row[-1]:
+                # Note the error
                 error += 1 
         error_rate.update({key: (error/size)})
 
 
 # Plot error rate
 def error_plot(rate):
+    # Unique k values
     x = sorted([int(x) for x in(set([k.split('-')[0] for k in rate.keys()]))])
+    # Manhattan
     y1 = [v for k,v in error_rate.items() if k.split('-')[1] == '1']
+    # Minkowski
     y2 = [v for k,v in error_rate.items() if k.split('-')[1] == '1.5']
+    # Euclidean
     y3 = [v for k,v in error_rate.items() if k.split('-')[1] == '2']
     plt.plot(x, y1, 'o:', label='manhattan')
     plt.plot(x, y2, 'o:', label='minkowski')
